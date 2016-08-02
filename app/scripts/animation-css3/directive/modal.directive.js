3a8b064c3750e29e44914f09cd6eb9463e86d2bc
cuongdv <cuong.duong@htklabs.com>
2016-08-02 18:14:51 +0700

    make directive modal

---
+++ app/scripts/animation-css3/directive/modal.directive.js
@@ -0,0 +1,27 @@
+(function () {
+	'use strict';
+	angular.module('myapp').directive('customModal',customModal);
+	customModal.$inject = ['$http','$compile'];
+	function customModal($http,$compile) {
+		return {
+			restrict: 'A',
+			transclude: true,
+			scope: {
+				ngModel: '='
+			},
+			// templateUrl: 'scripts/animation-css3/views/modal.template.html',
+			template: '',
+			link: function(scope, el, attrs) {
+				scope.modalId = attrs.modalId;
+				scope.title = attrs.modalTitle;
+				$http.jsonp(attrs.modalSrc);
+
+				scope.getContents = function(data){
+          $compile(data.contents)(scope, function(compliledElement, scope){
+              el.append(compliledElement);
+          });
+	      };
+			}
+		};
+	}
+})();
--- app/scripts/page3/controllers/calender.controller.js
+++ app/scripts/page3/controllers/calender.controller.js
@@ -7,5 +7,23 @@
 	CalenderCtrl.$inject = ['$scope'];
 	function CalenderCtrl($scope) {
 		$scope.day = new Date();
+
+		$scope.collapseData = [
+      {
+          title: "Collapse Group Item Title 1",
+          content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
+          collapsed: true
+      },
+      {
+          title: "Collapse Group Item Title 2",
+          content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
+          collapsed: false
+      },
+      {
+          title: "Collapse Group Item Title 2",
+          content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.",
+          collapsed: false
+      }
+    ];
 	}
 })();
